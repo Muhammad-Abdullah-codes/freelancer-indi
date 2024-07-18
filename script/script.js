b63cb74c8e7e1640342e7c802b56e4ece2b1bfa8
Muhammad-Abdullah-codes <mabdullahbhatti32@gmail.com>
2024-07-18 22:59:25 +0500

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,22 +1,22 @@
 
 const sectionHTML = `
-            <section class="bor mx-5 p-6 mb-5">
-    <div class="flex gap-5  mb-5">
-      <img src="img/Ellipse 2.png" alt="">
-      <div>
-        <p class="font-bold">Dr Ruwan Perera</p>
-        <p class="text-gray-400">Science</p>
-      </div>
-    </div>
-    <p class="text-blue-500 font-bold">Best Online English Medium Classes</p>
-    <p class="bg-gray-50 mt-2 p-2">
-      CLASSES~ENGLISH~HISTORY~ SCIENCE~HEALTH~CIVICS~ GEOGRAPHYFor each subject~Unit papers~Revision~Past papers~Colombo school papers~Short notesText or call on whatsapp for further details-</p>
-      <div class="flex gap-2 mt-4">
-        <button class="flex items-center but3 font-bold gap-2 bg-purple-100 px-3 py-1"><img width="18" src="img/dashboard.png" alt=""> Dashboard</button>
-        <button class="flex items-center but4 font-bold gap-2 bg-orange-100 px-3 py-1"><img width="18" src="img/dot.png" alt=""> Medium</button>
-      </div>
-      <p class="text-gray-400 mt-5">2024-12-12</p>
-  </section>
+            <section class="bor mx-5 p-6 mb-5 bg-white">
+        <div class="flex gap-5  mb-5">
+          <img src="img/Ellipse 2.png" alt="">
+          <div>
+            <p class="font-bold">Dr Ruwan Perera</p>
+            <p class="text-gray-400">Science</p>
+          </div>
+        </div>
+        <p class="text-blue-500 font-bold">Best Online English Medium Classes</p>
+        <p class="bg-gray-50 mt-2 p-2 rounded-xl">
+          CLASSES~ENGLISH~HISTORY~ SCIENCE~HEALTH~CIVICS~ GEOGRAPHYFor each subject~Unit papers~Revision~Past papers~Colombo school papers~Short notesText or call on whatsapp for further details-</p>
+          <div class="flex gap-2 mt-4">
+            <button class="flex items-center but3 font-bold gap-2 bg-purple-100 px-3 py-1"><img width="18" src="img/dashboard.png" alt=""> Dashboard</button>
+            <button class="flex items-center but4 font-bold gap-2 bg-orange-100 px-3 py-1"><img width="18" src="img/dot.png" alt=""> Medium</button>
+          </div>
+          <p class="text-gray-400 mt-5">2024-12-12</p>
+      </section>
         `;
 
 const container1 = document.getElementById('container1');
